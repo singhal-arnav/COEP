ca7a7bfc1dbd2ee44beb3a1d3c83aa573a426012
singhal_arnav <arnavsinghal2607@gmail.com>
2024-10-25 11:26:37 +0530

    Removed comment from infix to postfix conversion code

--- Lab Work - 10th September/Stack (infix to postfix conversion)/main.c
+++ Lab Work - 10th September/Stack (infix to postfix conversion)/main.c
@@ -1,18 +1,3 @@
-/*
-Algorithm to convert infix expression to postfix
-1. Declare a char Stack and initialize it
-2. Read the infix expression from L-R char by char
-3. if operand is encountered - append it to output string
-4. if '(' is encountered - push it in the stack
-5. if ')' pop all characters and appeend to output string till
-    ')' is encountered. DO not append ')'
-6. if operator is encountered pop all operators of higher or equal precedence and append to o/p
-    or till stack is not empty or ')' is encountered. drop ')'
-7. push the operator of step 6.
-8  repeat 2 - 6 till end of expression
-9 pop all characters and append.
-*/
-
 #include <stdio.h>
 #include <ctype.h>
 #include "stack.c"
