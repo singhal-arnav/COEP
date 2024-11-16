361bb8f0bc5c7d97d470a412e46d2368329c3838
singhal_arnav <arnavsinghal2607@gmail.com>
2024-11-16 19:22:24 +0530

    Completed Assignment 6

--- Assignment 6/heap.c
+++ Assignment 6/heap.c
@@ -3,86 +3,87 @@
 #include <limits.h>
 #include "heap.h"
 
-void init_heap(heap *h1, int size){  
-    h1->h = (int *)malloc(sizeof(int) * size);
-    h1->rear = -1;
-    h1->size = size;
+void init_heap(heap *h1, int size) {  
+    h1 -> h = (int *)malloc(sizeof(int) * size);
+    h1 -> rear = -1;
+    h1 -> size = size;
     return;
 }
 
-int parent(int index){       
-    return ((index-1)/2);
+int parent(int index) {       
+    return ((index - 1) / 2);
 }
 
-void swaph(heap *h1, int a, int b){ 
-    if(a>=h1->size || b >=h1->size)
+void swaph(heap *h1, int a, int b) { 
+    if(a >= h1 -> size || b >= h1 -> size)
         return;
     else{
-        int k=h1->h[a];
-        h1->h[a]=h1->h[b];
-        h1->h[b]=k;
+        int k = h1 -> h[a];
+        h1 -> h[a] = h1 -> h[b];
+        h1 -> h[b] = k;
         return;
     }
     return;
 }
-void insert_heap(heap *h1, int k){
-    if(h1->rear == h1->size-1)
+
+void insert_heap(heap *h1, int k) {
+    if(h1 -> rear == h1 -> size - 1)
         return;
-    h1->rear++;
-    h1->h[h1->rear]=k;
-    int i=h1->rear;
-    while(i > 0 && h1->h[i] > h1->h[parent(i)]){
+    h1 -> rear++;
+    h1 -> h[h1 -> rear] = k;
+    int i = h1 -> rear;
+    while(i > 0 && h1 -> h[i] > h1 -> h[parent(i)]) {
         swaph(h1, i, parent(i));
-        i=parent(i);
+        i = parent(i);
     }
     return;
 }
+
 void print_heap(heap h1){       
-    for(int i=0; i<=h1.rear; i++){
+    for(int i = 0; i <= h1.rear; i++)
         printf("%d\t", h1.h[i]);
-        
-    }
     printf("\n");
     return;
 }
-void heapify(heap *h1){          //Heapify using top down approch
-    if(h1->rear==0 || h1->rear==-1)
+
+void heapify(heap *h1) {    //Heapify using top down approch
+    if(h1 -> rear == 0 || h1 -> rear == -1)
         return;
-    int j=0;
-    while(j <=(h1->rear)){
-        int lchild=2*j+1;
-        int rchild=2*j+2;
-        if(lchild > h1->rear)
+    int j = 0;
+    while(j <= (h1 -> rear)){
+        int lchild = 2 * j + 1;
+        int rchild = 2 * j + 2;
+        if(lchild > h1 -> rear)
             return;
-        if(rchild > h1->rear){
-            if(h1->h[j] < h1->h[lchild]){
+        if(rchild > h1 -> rear){
+            if(h1 -> h[j] < h1 -> h[lchild]){
                 swaph(h1, j, lchild);
             }
             return;
         }
-        if((h1->h[j] > h1->h[lchild]) && (h1->h[j] > h1->h[rchild]))
+        if((h1 -> h[j] > h1 -> h[lchild]) && (h1 -> h[j] > h1 -> h[rchild]))
             return;
-        if(h1->h[lchild] > h1->h[rchild]){
+        if(h1 -> h[lchild] > h1 -> h[rchild]){
             swaph(h1, j, lchild);
-            j=lchild;
+            j = lchild;
         }
         else{
             swaph(h1, j, rchild);
-            j=rchild;
+            j = rchild;
         }
     }
 }
 
-void heap_sort(heap *h1){
-    int k=h1->rear;
-    if(h1->rear==-1)
+void heap_sort(heap *h1) {
+    int k = h1 -> rear;
+    if(h1 -> rear == -1)
         return;
-    for(int i=h1->rear; i>0; i--){
+    for(int i = h1 -> rear; i > 0; i--){
         swaph(h1,i,0);
-        h1->rear--;
+        h1 -> rear--;
         heapify(h1);
     }
-    h1->rear=k;
+    h1 -> rear = k;
     print_heap(*h1);
     return;
 }
--- Assignment 6/main.c
+++ Assignment 6/main.c
@@ -1,25 +1,49 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <string.h>
+#include <stdlib.h>
 #include "heap.h"
 
 #define MAX_HEAP_SIZE 100
+#define MAX_DIGITS 10
 
 int main(int argc, char **argv){
 	heap h1;
-	int fd, num;
+	int fd, i = 0, flag;
+	char buffer[MAX_DIGITS];
 
 	init_heap(&h1, MAX_HEAP_SIZE);
 
-	if((fd = open(argv[1], O_RDONLY)) != -1) {
-		while(read(fd, &num, sizeof(int))) {
-			insert_heap(&h1, num);
-		}
+	if((fd = open(argv[1], O_RDONLY)) == -1) {
+		perror("Error - File could not be opened\n");
+		return 1;
 	}
 	
+	while(read(fd, &buffer[i], 1)) {
+		if(buffer[i] >= 48 && buffer[i] <= 57) {
+			flag = 0;
+			i++;
+			continue;
+		}
+		if(!flag && strchr(", \t\n", buffer[i])) {
+			buffer[i] = '\0';
+			i = 0;
+			insert_heap(&h1, atoi(buffer));
+			flag = 1;
+			continue;
+		}
+	}
+
+	if (i > 0 && !flag) {
+		buffer[i] = '\0';
+		insert_heap(&h1, atoi(buffer));
+	}
+	
+	close(fd);
+
 	print_heap(h1);
 	heap_sort(&h1);
 	print_heap(h1);
-	
+
 	return 0;
 }
